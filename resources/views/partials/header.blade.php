294beced10cab4e08fec038aeaa866b46dbc45cf
Edoardo <ercolino.edoardo@gmail.com>
2021-10-27 16:24:40 +0200

    Scaffolding

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,18 @@
+<header>
+    {{-- header top --}}
+    <div class="line">
+        <div class="container-line">
+            <p>DC POWER<sup>sm</sup> VISA®</p>
+            <p>ADDICIONAL DC SITES</p>
+        </div>
+    </div>
+
+    {{-- header bottom --}}
+    <div class="container-header">
+        <div class="logo">
+            <img src="img/dc-logo.png" alt="logo">
+        </div>
+        <div class="nav"></div>
+        <div class="search"></div>
+    </div>
+</header>
